68f8f1c0cbad05b9a5ca6f471661d4721cb6f39e
Mariano <Mariano.roldan12@gmail.com>
2018-10-20 12:45:54 -0700

    Better COMMS Stuff

--- COMMS data.py
+++ COMMS data.py
@@ -1,9 +1,12 @@
 # Units are in milliWatt
 # Time is in seconds
 
+# States
 COMMS_transmit = 0
 COMMS_sleep = 1
 COMMS_idle = 0
+Sleep_out = 0;
+
 state_timer = 0
 
 hours = 60 * 60
@@ -17,17 +20,28 @@ idle_time = 90 * 60 - 90  # For all time except for 90s transmission
 # Sleep during launch
 sleep_time = 60 * 30  # 30 minutes
 sleep_power = 0
-sleep_out_time = 60  # assumed
 
 # Transmit Picture
 transmit_time = 90
 transmit_power = 3410  # Max and typical are similar values
 
-def commsPower():
 
+def commsPower():
     global COMMS_transmit
     global COMMS_sleep
     global COMMS_idle
+    global state_timer
+
+    if COMMS_sleep == 1 and Sleep_out == 1:
+        COMMS_sleep = 0
+        COMMS_idle = 1
+
+    elif COMMS_transmit == 1 and state_timer >= transmit_time:
+        COMMS_transmit = 0
+        COMMS_idle = 1
+        state_timer = 0
+
+    state_timer += 1
 
     if COMMS_transmit:
         return transmit_power
@@ -37,25 +51,3 @@ def commsPower():
 
     elif COMMS_idle:
         return idle_power_typical
-
-def resetFlag():
-
-    global COMMS_transmit
-    global COMMS_sleep
-    global COMMS_idle
-    global state_timer
-
-    state_timer += 1
-
-    if (COMMS_sleep == 1 and state_timer >= sleep_out_time):
-        COMMS_sleep = 0
-        COMMS_idle = 1
-        state_timer = 0
-        return
-
-    elif(COMMS_transmit == 1 and state_timer >=transmit_time):
-        COMMS_transmit = 0
-        COMMS_idle = 1
-        state_timer = 0
-        return
-
